a0aac93fae028d5c129126c950b1ea845cc82b5e
yuxxeun <itsyuxxeun@gmail.com>
2022-07-10 15:08:57 +0700

    fix: footer url

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,7 +2,7 @@
   <div>
     <p class="text-sm d-flex font-basement tracking-wide mb-5 mt-10 text-white text-center">
       ✨ MIT &bull; SvelteKit &bull; <a
-        href="github.com/yuxxeun"
+        href="https://github.com/yuxxeun"
         class="underline">@yuxxeun</a
       > 🔮
     </p>
